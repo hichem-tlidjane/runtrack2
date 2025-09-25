86c306af08e2042b7cbd2afcc8eb9bdcf674b51a
hichem-tlidjane <hichem.tlidjane@laplateforme.io>
2025-09-25 10:42:10 +0200

    jour09 complet

---
+++ jour09/job15/job15.sql
@@ -0,0 +1,7 @@
+
+-- Ecrivez dans le fichier “job15.sql” une requête permettant de récupérer le nom des
+-- salles et le nom de leur étage.
+
+SELECT salles.nom AS nom_salle, etage.nom AS nom_etage
+FROM salles
+JOIN etage ON salles.id_etage = etage.id;
---
+++ jour09/job16/job16.sql
@@ -0,0 +1,9 @@
+-- Ecrivez dans le fichier “job16.sql” une requête permettant de récupérer le nom de
+-- l'étage ayant la salle avec la plus grande capacité (et afficher aussi le nom de cette salle
+-- ainsi que sa capacité).
+-- Dans ce résultat, la colonne “nom” de la salle doit être renommée en “Biggest Room”
+
+SELECT etage.nom AS nom_etage, salles.nom AS "Biggest Room", salles.capacite
+FROM salles
+JOIN etage ON salles.id_etage = etage.id
+WHERE salles.capacite = (SELECT MAX(capacite) FROM salles);
